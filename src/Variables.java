a161b7a2b3a682410083574c063b41e545433ed4
Ramanan-B <ramramanan075@gmail.com>
2026-07-24 11:48:34 +0530

    first commit

--- src/Variables.java
+++ src/Variables.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public static class Variables {
 
-public class Variables {
+    int a = 10; // instance variable  its scope in class
+    static int b = 20; // static variable class level shared all the object of the class
+    int c = a + b;
+
+    void display() {
+        int d = 20; // local variables its scope it method
+        System.out.println(d);
+    }
+
+
 }
+  public static void main(String[] args) {
+//        Variables v = new Variables();
+//      System.out.println(v.c);
+//
+//      v.display();
+      System.out.println(Variables.b);
+
+  }
+
